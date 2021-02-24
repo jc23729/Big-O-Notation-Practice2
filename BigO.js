8b7a50e17769a11786edb2ee398097fccdf65385
Juan Cruz <jc23729@gmail.com>
2021-02-24 14:21:56 -0500

    update

--- BigO.js
+++ BigO.js
@@ -13,6 +13,8 @@
 // O(5 + 3 + 1)
 // O(n + n^(1/2) + n^2 + n * log(n)^10)
 
+
+////////////////////Product Rule///////////////////////////////////
 //constanst are numbers
 
 0(4*n)= 0(n)
@@ -20,6 +22,17 @@
 
 //because 1/3 is still
 // a number or cosntant value so you can drop it
-0(n/3) = 0(n / 1/3) = 0 (n)
+O(n/3) = O(n / 1/3) = O(n)
 //product rule example in this case your just rewriting it to 0^2 power
 0(5*n*n)= 0(n*n) = O(n^2)
+0(765) = O(1)
+
+
+/////////////////////Sum rule /////////////////////////////////
+//is the sum of multipe terms, only keep the largest term, drop the rest
+// N can be any variable, could be 1 or 10000
+O(n+1000) = 0(n)
+//just choose the term thats bigger n squared is bigger
+O(n^2 +n) = O(n^2)
+
+O(n + 500 + n^3 + n^2)
