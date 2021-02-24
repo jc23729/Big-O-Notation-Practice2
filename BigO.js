a5f66ab136c9ce35881b01f22c1a89601a574605
Juan Cruz <jc23729@gmail.com>
2021-02-24 14:08:36 -0500

    update

--- BigO.js
+++ BigO.js
@@ -1,6 +1,6 @@
 // Step One: Simplifying Expressions
 // Simplify the following big O expressions as much as possible:
-
+//Product rule If Big o is the product of multiple terms, drop constant terms
 // O(n + 10)
 // O(100 * n)
 // O(25)
@@ -11,3 +11,11 @@
 // O(2^n + n^2)
 // O(5 + 3 + 1)
 // O(n + n^(1/2) + n^2 + n * log(n)^10)
+
+//constanst are numbers
+
+0(4*n)= 0(n)
+0(512*n)=0(n)
+
+product rule
+0(n/3) = 0(1/3 )
