8c7c502d0b7e9635aec72262f1578d75d8ac209d
Juan Cruz <jc23729@gmail.com>
2021-02-25 21:35:41 -0500

    update

--- Big-O-practice.js
+++ Big-O-practice.js
@@ -22,29 +22,31 @@ O(1)
 // O(n + n^(1/2) + n^2 + n * log(n)^10)
 O(n^2)
 
-Step Two: Calculating Time Complexity
-Determine the time complexities for each of the following functions. If you’re not sure what these functions do, copy and paste them into the console and experiment with different inputs!
+// Step Two: Calculating Time Complexity
+// Determine the time complexities for each of the following functions. If you’re not sure what these functions do, 
+// copy and paste them into the console and experiment with different inputs!
 
 function logUpTo(n) {
   for (let i = 1; i <= n; i++) {
     console.log(i);
   }
 }
-Time Complexity: O(n)
+// Time Complexity: O(n)
+
 
 function logAtLeast10(n) {
   for (let i = 1; i <= Math.max(n, 10); i++) {
     console.log(i);
   }
 }
-Time Complexity: O(n)
+// Time Complexity: O(n)
 
 function logAtMost10(n) {
   for (let i = 1; i <= Math.min(n, 10); i++) {
     console.log(i);
   }
 }
-Time Complexity: O(1)
+// Time Complexity: O(1)
 
 function onlyElementsAtEvenIndex(array) {
   let newArray = [];
@@ -55,7 +57,7 @@ function onlyElementsAtEvenIndex(array) {
   }
   return newArray;
 }
-Time Complexity: O(n)
+// Time Complexity: O(n)
 
 function subtotals(array) {
   let subtotalArray = [];
@@ -68,7 +70,7 @@ function subtotals(array) {
   }
   return subtotalArray;
 }
-Time Complexity: O(n^2)
+// Time Complexity: O(n^2)
 
 function vowelCount(str) {
   let vowelCount = {};
@@ -86,5 +88,21 @@ function vowelCount(str) {
 
   return vowelCount;
 }
-Time Complexity: O(n) // includes is on a fixed length string // the max number of keys is 10
+// Time Complexity: O(n) // includes is on a fixed length string // the max number of keys is 10
 
+art 3 - short answer
+Answer the following questions
+
+// True or false: n^2 + n is O(n^2). True
+// True or false: n^2 * n is O(n^3). True
+// True or false: n^2 + n is O(n). False
+// What’s the time complexity of the .indexOf array method? O(n)
+// What’s the time complexity of the .includes array method? O(n)
+// What’s the time complexity of the .forEach array method? O(n) at least (depends on what the callback does)
+// What’s the time complexity of the .sort array method? O(n log n)
+// What’s the time complexity of the .unshift array method? O(n)
+// What’s the time complexity of the .push array method? O(1)
+// What’s the time complexity of the .splice array method? O(n) it can be O(1) if the end, but we can’t assume that
+// What’s the time complexity of the .pop array method? O(1)
+// What’s the time complexity of the Object.keys() function? O(n)
+// What’s the space complexity of the Object.keys() function? O(n)
